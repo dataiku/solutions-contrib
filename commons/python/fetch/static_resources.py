143bab57b74c923571cc775ff76233ae9d672fa3
abdelmajid.boubrik@dataiku.com <abdelmajid.boubrik@dataiku.com>
2022-06-15 10:42:13 +0000

    Fixed getting commons lib folder from python

--- commons/python/fetch/static_resources.py
+++ commons/python/fetch/static_resources.py
@@ -1,13 +1,70 @@
+import os
+from flask import Response
+from flask import Flask
+from pathlib import Path
+from flask import Blueprint
+import datetime
 
-project_lib_dir = os.environ.get('PYTHONPATH', '').split(os.pathsep)[-1]
+
+''''
+to enable fetching resources from your web app backend include the following 
 
 
-@app.route('/static/<resource_type>/<resource_lib_name>/<resource_version>/<resource_file_name>')
-@app.route('/static/<resource_type>/<resource_file_name>')
-@app.route('/static/<resource_file_name>')
+from commons.python.fetch.static_resources import fetch_route
+app.register_blueprint(fetch_route)
+
+The route for fetching resources will be declared in flask application
+
+TODO: add possibility to configure cache control by resource type 
+
+''''
+
+
+def get_commons_lib_directory():
+    for lib_dir in os.environ.get('PYTHONPATH', '').split(os.pathsep):
+        if lib_dir.endswith('python/commons'):
+            return lib_dir
+    return None 
+       
+            
+fetch_route = Blueprint('fetch_route', __name__)
+sol_commons_lib_dir  = get_commons_lib_directory()
+
+
+
+def fetch_resource(lib_path, resource_type, resource_lib_name, resource_version, resource_file_name):
+    path = Path(lib_path)
+    content = None
+    status = 200
+    if resource_type:
+        path = path.joinpath(resource_type)
+    if resource_lib_name:
+        path = path.joinpath(resource_lib_name)
+    if resource_version:
+        path = path.joinpath(resource_version)
+    path = path.joinpath(resource_file_name)
+    try:
+        with open(path, 'rb') as f:
+            content = f.read()
+    except FileNotFoundError as err:
+        status = 404  # not found
+        print("FileNotFoundError: {0}".format(err))
+    except PermissionError as err:
+        print("PermissionError: {0}".format(err))
+        status = 403  # forbidden
+    except OSError as err:
+        print("OSError: {0}".format(err))
+        status = 500  # Generic internal error
+
+    return {'status': status, 'content': content}
+
+
+@fetch_route.route('/static/<resource_type>/<resource_lib_name>/<resource_version>/<resource_file_name>')
+@fetch_route.route('/static/<resource_type>/<resource_file_name>')
+@fetch_route.route('/static/<resource_file_name>')
 def get_static_resource(resource_file_name, resource_type=None, resource_lib_name=None, resource_version=None):
-    resource = fetch_resource(project_lib_dir, resource_type, resource_lib_name, resource_version, resource_file_name)
-    file_name, file_extension = os.path.splitext('/path/to/somefile.ext')
+    resource = fetch_resource(sol_commons_lib_dir, resource_type, resource_lib_name, resource_version, resource_file_name)
+    file_name, file_extension = os.path.splitext(resource_file_name)
     mime_type = 'text/html'
     if resource_type == 'js' or file_extension == 'js':
         mime_type = 'text/javascript'
