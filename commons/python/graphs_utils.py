6f9b234f006ea7a43c5302c3eb2e2468136682c7
antoinedetorcy <antoine.detorcy@datailu.com>
2022-06-21 15:31:29 +0200

    first draft of graphs_utils #25

---
+++ commons/python/graphs_utils.py
@@ -0,0 +1,19 @@
+class Graph:
+    def __init__(self, graph_name, graph_type, dom_id):
+        self.name = graph_name
+        self.type = graph_type
+        self.dom_id = dom_id
+
+
+class LineGraph(Graph):
+    def __init__(self, graph_name, dom_id, x_values, y_values):
+        Graph.__init__(self, graph_name, "line", dom_id)
+        self.x_values = x_values
+        self.y_values = y_values
+
+
+class DashBoard:
+    def __init__(self, title, status, graphs):
+        self.title = title
+        self.status = status
+        self.graphs = graphs
