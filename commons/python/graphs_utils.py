c7c4dc029ca4a0b97f7879017a9249edd4969cf3
antoinedetorcy <antoine.detorcy@datailu.com>
2022-06-22 17:44:11 +0200

    add bar series #25

--- commons/python/graphs_utils.py
+++ commons/python/graphs_utils.py
@@ -5,16 +5,20 @@ class Dashboard:
 
 
 class Chart:
-    def __init__(self, graph_title, canvas_type, series, dom_id):
+    def __init__(self, graph_title, type, series, dom_id, graph_subtitle=None):
         self.title = graph_title
-        self.type = canvas_type
+        self.subtitle = graph_subtitle
         self.dom_id = dom_id
         self.series = series
-
+        if type is not None:
+            self.type = type
+        else:
+            self.type = 'base'
 
 class XYChart(Chart):
-    def __init__(self, graph_name, dom_id, x_type, y_type, series, x_axis_values=None, y_axis_values=None):
-        Chart.__init__(self, graph_name, 'XYChart', series, dom_id)
+    def __init__(self, graph_name, dom_id, x_type, y_type, series, x_axis_values=None, y_axis_values=None,
+                 graph_subtitle=None):
+        Chart.__init__(self, graph_name, 'XYChart', series, dom_id, graph_subtitle)
         self.x_type = x_type  # either 'value', 'category', 'time', 'log'
         self.y_type = y_type  # either 'value', 'category', 'time', 'log'
         self.x_axis_values = x_axis_values
@@ -23,7 +27,7 @@ class XYChart(Chart):
 
 class Series:
     def __init__(self, data, legend_name=None):
-        self.legend_name = legend_name
+        self.name = legend_name
         self.data = data
 
 
@@ -41,3 +45,10 @@ class BarSeries(Series):
         self.type = 'bar'
         if stack is not None:
             self.stack = 'Total'
+
+
+class PieSeries(Series):
+    def __init__(self, slice_names, slice_values):
+        data = {"name": slice_names, "value": slice_values}
+        Series.__init__(self, data)
+        self.type = 'pie'
