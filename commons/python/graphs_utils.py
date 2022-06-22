38fb02f8f1f205d83b741400c2a9d56a3936322a
antoinedetorcy <antoine.detorcy@datailu.com>
2022-06-22 12:00:39 +0200

    add series object #25

--- commons/python/graphs_utils.py
+++ commons/python/graphs_utils.py
@@ -1,19 +1,30 @@
-class Graph:
-    def __init__(self, graph_title, graph_type, dom_id):
+class Chart:
+    def __init__(self, graph_title, canvas_type, series, dom_id):
         self.title = graph_title
-        self.type = graph_type
+        self.type = canvas_type
         self.dom_id = dom_id
+        self.series = series
 
 
-class LineGraph(Graph):
-    def __init__(self, graph_name, dom_id, x_values, y_values):
-        Graph.__init__(self, graph_name, "line", dom_id)
-        self.x_values = x_values
-        self.y_values = y_values
+class XYChart(Chart):
+    def __init__(self, graph_name, graph_type, dom_id, x_type, y_type, series, x_axis_values=None, y_axis_values=None):
+        Chart.__init__(self, graph_name, 'XYChart', series, dom_id)
+        self.x_type = x_type  # either 'value', 'category', 'time', 'log'
+        self.y_type = y_type  # either 'value', 'category', 'time', 'log'
+        self.x_axis_values = x_axis_values
+        self.y_axis_values = y_axis_values
+
+
+class LineSeries:
+    def __init__(self, legend_name, data, stack=None):
+        self.legend_name = legend_name
+        self.type = 'line'
+        self.data = data
+        if stack is not None:
+            self.stack = 'Total'
 
 
 class Dashboard:
-    def __init__(self, title, status, graphs):
+    def __init__(self, title, charts):
         self.title = title
-        self.status = status
-        self.graphs = graphs
+        self.charts = charts
