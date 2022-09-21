170304b23b48854122a2c0d7c3ca09eb57e85b6a
Anas Laaroussi <anaslaaroussi@MacBook-Pro-de-Anas-2.local>
2022-09-21 18:49:15 +0200

    abstract dev & dss env modes

---
+++ commons/python/fetch/config_bs.py
@@ -0,0 +1,84 @@
+
+from os.path import expanduser, exists, join, dirname, abspath
+import os
+from enum import Enum
+
+
+# TODO : Resolve https / http replacement 
+
+class EnvMode(Enum):
+    LOCAL = "local"
+    DSS = "dss"
+
+class ConfigBs(object):
+
+    project_folder_name = "project"
+    html_file_name = "index.html"
+    arg_url_name = "URL"
+
+
+    @staticmethod
+    def __get_env_mode():
+        is_local = exists(join(expanduser("~"),".dataiku/bs-config.json"))
+        return EnvMode.LOCAL.value if is_local else EnvMode.DSS.value
+    
+    @classmethod
+    def __get_lib_python_path(cls):
+        mode = cls.__get_env_mode()
+        if mode == EnvMode.DSS.value:
+            for lib_dir in os.environ.get("PYTHONPATH", "").split(os.pathsep):
+                if lib_dir.endswith("python/commons"):
+                    return dirname(lib_dir)
+ 
+        return dirname(dirname(dirname(dirname(abspath(__file__)))))
+    
+    @classmethod
+    def __get_project_entry_file(cls):
+        python_path = cls.__get_lib_python_path()
+        project_dir = join(python_path,cls.project_folder_name)
+        file_dir = join(project_dir,cls.html_file_name)
+        if exists(file_dir):
+            return file_dir
+        return None
+    
+    @classmethod
+    def __get_dir_name(cls):
+        mode = cls.__get_env_mode()
+        if mode == EnvMode.DSS.value:
+            return "python"
+        else:
+            python_lib_path = cls.__get_lib_python_path() 
+            return python_lib_path.split("/")[-1]
+    
+    @classmethod
+    def __get_lib_backend_url(cls,request):
+        dir_name = cls.__get_dir_name()
+        lib_url = None
+        if cls.__get_env_mode() == EnvMode.DSS.value:
+            lib_url = request.url_root + request.args.get(cls.arg_url_name)[1:] + dir_name
+        else:
+            lib_url = request.url_root + dir_name
+        
+        return lib_url
+    
+    @classmethod
+    def static_folder(cls):
+        return cls.__get_lib_python_path()
+    
+    @classmethod
+    def template_folder(cls):
+        return dirname(cls.__get_project_entry_file())
+    
+    @classmethod
+    def html_file(cls):
+        return cls.__get_project_entry_file()
+    
+    @classmethod
+    def lib_backend_url(cls, request):
+        return cls.__get_lib_backend_url(request)
+    
+
+
+if __name__ == "__main__":
+    print(ConfigBs.static_folder())
+    print(ConfigBs.template_folder())
--- commons/python/fetch/fetch_project.py
+++ commons/python/fetch/fetch_project.py
@@ -2,98 +2,28 @@ from flask import Blueprint, Response, make_response, render_template, request
 import os
 import datetime
 import logging
+from .config_bs import ConfigBs
 
 
 ## Setup logger
 logger = logging.getLogger(__name__)
 
-
-def get_lib_python_path_mode():
-    """
-    """
-    for lib_dir in os.environ.get("PYTHONPATH", "").split(os.pathsep):
-        if lib_dir.endswith("python/commons"):
-            return {"path" : os.path.dirname(lib_dir), "mode" : "dss"}
- 
-    return {"path" : os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))), "mode" : "local"}
-
-PYTHON_PATH_MODE = get_lib_python_path_mode()
-PYTHON_MODE = PYTHON_PATH_MODE["mode"]
-PYTHON_PATH = PYTHON_PATH_MODE["path"]
+# TODO : Node js server (src / build ) 
 
 
-def get_project_entry_html(
-        project_folder="project",
-        html_file="index.html",
-        python_lib_path=PYTHON_PATH
-    ):
-        """
-        """
-        if python_lib_path:
-            project_dir = os.path.join(python_lib_path,project_folder)
-            file_dir = os.path.join(project_dir,html_file)
-            if os.path.exists(file_dir):
-                return file_dir
-            return None
-        return None
+fetch_route =  Blueprint("fetch_route", __name__,template_folder=ConfigBs.template_folder(),static_folder=ConfigBs.static_folder())
 
-def get_dir_name(
-    mode=PYTHON_MODE,
-):
-    if mode == "dss":
-        return "python"
-    else:
-        return PYTHON_PATH.split("/")[-1]
-
-
-DIR_NAME = get_dir_name()
-
-
-def get_project_entry_html(
-    project_folder="project",
-    html_file="index.html",
-    python_lib_path=PYTHON_PATH
-):
-    """
-    """
-    if python_lib_path:
-        project_dir = os.path.join(python_lib_path,project_folder)
-        file_dir = os.path.join(project_dir,html_file)
-        if os.path.exists(file_dir):
-            return file_dir
-        return None
-    return None
-
-PATH_TO_INDEX_FILE = get_project_entry_html()
-
-fetch_route = Blueprint("fetch_route", __name__,static_folder=PYTHON_PATH) if PATH_TO_INDEX_FILE is None else Blueprint("fetch_route", __name__,template_folder=os.path.dirname(PATH_TO_INDEX_FILE),static_folder=PYTHON_PATH)
-
-
-def get_lib_backend_url(request,
-            mode,
-            dir_name=DIR_NAME,
-            arg_url_name="URL",
-            ):
-    lib_url = None
-    if mode == "dss":
-        lib_url = request.url_root + request.args.get(arg_url_name)[1:] + dir_name
-        return lib_url.replace("http","https")
-    else:
-        lib_url = request.url_root + dir_name
-        return lib_url
-    
-    
-    
 
 @fetch_route.route('/fetch/bs_init')
 def init_project():
-    lib_url = get_lib_backend_url(request,PYTHON_MODE)
+    lib_url = ConfigBs.lib_backend_url(request)
+    html_file_path = ConfigBs.html_file()
     logger.info("lib URL : " + lib_url)
     content = None
     status = 200
-    if PATH_TO_INDEX_FILE:
+    if html_file_path:
         try:
-            with open(PATH_TO_INDEX_FILE,"rb") as f:
+            with open(html_file_path,"rb") as f:
                 content = f.read()
         except PermissionError as err:
             logger.error("PermissionError: {0}".format(err))
