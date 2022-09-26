8ec1ed23c79bf651c14ccd0ad5e5b7121032064a
anas laaroussi <anas.laaroussi@dataiku.com>
2022-09-26 15:25:12 +0200

    script init project

---
+++ commons/scripts/init_bs_project.sh
@@ -0,0 +1,44 @@
+#!/bin/bash 
+
+
+source_zip="https://github.com/dataiku/solutions-contrib/archive/refs/heads/web-infra.zip"
+
+# get project name and remote git url 
+read -p "Enter your project name:"  projectname
+read -p "Enter your remote git url for this project:" remote_git_url
+
+
+# download zip 
+wget "$source_zip" -O "$projectname-root.zip"
+
+# get folder name in zip file
+source_folder_name=$(unzip -Z -1 "$projectname-root.zip" | head -1)
+
+# unzip and rename project
+unzip ./"$projectname-root.zip" && \
+rm ./"$projectname-root.zip" && \
+mv ./"$source_folder_name" ./"$projectname-workspace" && \
+mv ./"$projectname-workspace/project"  ./"$projectname-workspace/${projectname}"
+
+# Add commons to .gitignore
+echo "\ncommons" >> ./"$projectname-workspace/.gitignore"
+
+# Attach project to new remote
+cd ./"$projectname-workspace/" && \
+git init && \
+git add . && \
+git commit -m "initial setup" && \
+git branch -M main && \
+git remote add origin "$remote_git_url" && \
+git push -u origin main
+
+
+# Install dependencies
+## Intall Node & npm 
+
+
+
+## Install python & virtualenv setup 
+
+
+
