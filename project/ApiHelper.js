23ad925b43fe4e824ba5f862939ede23cc71e195
anas laaroussi <anas.laaroussi@dataiku.com>
2022-10-05 16:25:57 +0200

    api helper & dev / prod mode

---
+++ project/ApiHelper.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+
+const mode = process.env.NODE_ENV
+
+axios.defaults.baseURL = mode === "production" ?  getWebAppBackendUrl('') : "http://127.0.0.1:5000";
+
+axios.interceptors.response.use((response) => {
+    return response.data
+}, (error) => {
+    APIErrors.push(error.response);
+    return Promise.reject(error);
+})
+
+
+export let APIErrors = [];
+
+export let InspectorAPI = {}
