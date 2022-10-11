5c5ce459ea052539a09425d2ed8abb7e03ebc6b2
Malick KONATE <gkonate.malick@gmail.com>
2022-10-11 16:30:46 +0200

    Update clustering.py

--- commons/python/dku_utils/visual_ml/clustering.py
+++ commons/python/dku_utils/visual_ml/clustering.py
@@ -2,8 +2,7 @@ def get_per_clusters_features_distribution_data(ml_task, model_id):
     """
     Retrieves the distribution information of a clustering modeling features, for each learned cluster.
         
-    :param: ml_task_settings: dataikuapi.dss.ml.DSS[MLTaskType]MLTaskSettings: DSS MLTask settings object.
-        '[MLTaskType]' varies depending on the type of MLTask (Regression, Classification, Clustering).
+    :param: ml_task_settings: dataikuapi.dss.ml.DSSClusteringMLTaskSettings: DSS MLTask settings object.
     :param model_id: str: ID of the model from which we want to retrieve the information, as we get from
         'ml_task.get_trained_models_ids()'.
     
@@ -31,3 +30,23 @@ def get_per_clusters_features_distribution_data(ml_task, model_id):
         pass
     pass
     return per_clusters_features_distribution_data
+
+
+def rename_dss_default_clusters(ml_task, cluster_renamings):
+    """
+    Renames default clusters computed by DSS. 
+    
+    :param: ml_task_settings: dataikuapi.dss.ml.DSSClusteringMLTaskSettings: DSS MLTask settings object.
+    :param: clusters_renaming: dict: Dictionary containing the mapping between the default cluster labels 
+        and their new names. 
+        Example = {'cluster_0': 'new_name_for_cluster_0',
+                   'cluster_1': 'new_name_for_cluster_1',
+                   'cluster_2': 'new_name_for_cluster_2'}
+    
+    """
+    model_details = ml_task.get_trained_model_details(model_id)
+    metadata_set_by_user = model_details.get_user_meta()
+    for dss_cluster_name in cluster_renamings.keys():
+        model_details.get_user_meta()["clusterMetas"][dss_cluster_name]["name"] = cluster_renamings[dss_cluster_name]
+    model_details.save_user_meta()
+    pass
