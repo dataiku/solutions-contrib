dffccee9b9fecda4514a9796c024c7117c23b4ef
Malick KONATE <gkonate.malick@gmail.com>
2022-10-19 22:50:09 +0200

    Update join_recipe.py

--- commons/python/dku_utils/recipes/join_recipe.py
+++ commons/python/dku_utils/recipes/join_recipe.py
@@ -127,7 +127,8 @@ class programmaticJoinHandler:
         pass
 
     def initialize_post_join_filter_expression(self):
-        self.recipe_payload["postFilter"] = []
+        self.recipe_payload["postFilter"] = {}
+        self.recipe_payload["postFilter"]["uiData"] = {}
         self.recipe_payload["postFilter"]["uiData"]["conditions"] = []
         self.recipe_payload["postFilter"]["expression"] = ""
         self.recipe_payload["postFilter"]["enabled"] = False
