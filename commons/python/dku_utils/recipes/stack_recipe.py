5f85afb563606caee7d74138fb84e8c7fb777fd1
Malick KONATE <gkonate.malick@gmail.com>
2022-10-19 07:17:27 +0200

    Update stack_recipe.py

--- commons/python/dku_utils/recipes/stack_recipe.py
+++ commons/python/dku_utils/recipes/stack_recipe.py
@@ -4,6 +4,13 @@ from ..datasets.dataset_commons import create_dataset_in_connection
 
 def instantiate_stack_recipe(project, recipe_name, recipe_input_datasets,
                              recipe_output_dataset_name, connection_name):
+    """
+    :param project: dataikuapi.dss.project.DSSProject: A handle to interact with a project on the DSS instance.
+    :param recipe_name: str: Name of the recipe.
+    :param recipe_input_datasets: list: List containing all recipe input dataset names. 
+    :param recipe_output_dataset_name: str: Name of the dataset that msut be the recipe output.
+    :param :connection_name: str: Name of the recipe output dataset connection.
+    """
     print("Creating stack recipe '{}' ...".format(recipe_name))
     builder = dataikuapi.StackRecipeCreator(recipe_name, project)
     for dataset_name in recipe_input_datasets:
