b1cff68470f155f3fa1f8534d058e907955b8535
anas laaroussi <anas.laaroussi@dataiku.com>
2022-10-20 18:43:43 +0200

    add button styles

---
+++ commons/_shared/bs-lib-components/ui/dev/src/pages/BsButton.vue
@@ -0,0 +1,14 @@
+<template>
+    <q-page padding>
+        <p class="q-mt-md">Primary</p>
+        <div class="q-gutter-sm">
+            <q-btn class="bs-btn dku-text" unelevated>Fullscreen</q-btn>
+            <q-btn class="bs-btn dku-text" unelevated icon="fullscreen">Fullscreen</q-btn>
+            <q-btn class="bs-btn dku-text" unelevated icon-right="fullscreen">Fullscreen</q-btn>
+            <q-btn class="bs-btn dku-text bs-btn--disabled" unelevated disable="">Fullscreen</q-btn>
+        </div>
+        <p class="q-mt-md">Secondary</p>
+    </q-page>
+</template>
+<script>
+</script>
---
+++ commons/_shared/bs-lib-components/ui/src/components/BsButton.vue
@@ -0,0 +1,23 @@
+<template>
+    
+</template>
+<script>
+    export default {
+        name: "BsButton",
+        props: {
+            variant: {
+                type: String,
+                validator: (value) => {
+                    return [
+                        "primary",
+                        "secondary",
+                        "link"
+                    ].indexOf(value) >= 0
+                }
+            },
+            label: {
+                type: String
+            }
+        }
+    }
+</script>
