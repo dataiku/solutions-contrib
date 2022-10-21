8b466371110c6c547b26cce06603e2174ec8fbd3
Malick KONATE <gkonate.malick@gmail.com>
2022-10-21 17:42:32 +0200

    Update prepare_recipe.py

--- commons/python/dku_utils/recipes/prepare_recipe.py
+++ commons/python/dku_utils/recipes/prepare_recipe.py
@@ -16,3 +16,25 @@ def compute_prepare_rename_step(column_to_rename, new_column_name):
                         "to": "{}".format(new_column_name)}]},
                    "alwaysShowComment": False}
     return rename_step
+
+
+def compute_prepare_formula_step(column_name, formula_expression):
+    """
+    Generates a column applying a prepare recipe formula.
+
+    :param column_to_rename: str: Name of the column to generate.
+    :param formula_expression: str: Expression of the formula leading to the computed column, following the 
+        DSS formula language (https://doc.dataiku.com/dss/latest/formula/index.html).
+    
+    :returns: formula_step: dict: JSON of the prepare 'formula' step.
+    """
+    formula_step = {'preview': False,
+                    'metaType': 'PROCESSOR',
+                    'disabled': False,
+                    'type': 'CreateColumnWithGREL',
+                    'params': {
+                        'expression': formula_expression,
+                        'column': column_name
+                        },
+                    'alwaysShowComment': False}
+    return formula_step
