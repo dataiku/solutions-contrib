e8c6e92785717a6f084bc975d54d3933dcadd6cb
Malick KONATE <gkonate.malick@gmail.com>
2022-10-21 18:46:30 +0200

    Update prepare_recipe.py

--- commons/python/dku_utils/recipes/prepare_recipe.py
+++ commons/python/dku_utils/recipes/prepare_recipe.py
@@ -20,7 +20,7 @@ def compute_prepare_rename_step(column_to_rename, new_column_name):
 
 def compute_prepare_formula_step(column_name, formula_expression):
     """
-    Generates a column applying a prepare recipe formula.
+    Computes the JSON associated with a column applying a prepare recipe formula.
 
     :param column_to_rename: str: Name of the column to generate.
     :param formula_expression: str: Expression of the formula leading to the computed column, following the 
@@ -37,4 +37,27 @@ def compute_prepare_formula_step(column_name, formula_expression):
                         'column': column_name
                         },
                     'alwaysShowComment': False}
+    return formula_step
+
+
+def compute_prepare_keep_or_delete_step(columns_of_interest, bool_keep_columns):
+    """
+    Computes the JSON associated with a keep/delete prepare recipe step.
+
+    :param columns_of_interest: list: Name of the column to generate.
+    :param bool_keep_columns: bool: Parameter precising if the columns mentioned in 
+        'columns_of_interest' should be kept (if it is equal to 'True')
+        or deleted (if it is equal to 'False').
+    
+    :returns: formula_step: dict: JSON of the prepare 'keep/delete' step.
+    """
+    formula_step = {'preview': False,
+                    'metaType': 'PROCESSOR',
+                    'disabled': False,
+                    'type': 'ColumnsSelector',
+                    'params': {'columns': columns_of_interest,
+                     'keep': bool_keep_columns,
+                     'appliesTo': 'COLUMNS'},
+                    'alwaysShowComment': False}
+
     return formula_step
