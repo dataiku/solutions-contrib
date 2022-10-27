d1b288f179a1d82d4ab82489fe6e41426ed6c714
anas laaroussi <anas.laaroussi@dataiku.com>
2022-10-27 11:07:42 +0200

    Bs Tooltips [sc-105723]

---
+++ commons/_shared/bs-lib-components/ui/dev/src/pages/BsTooltip.vue
@@ -0,0 +1,33 @@
+<template>
+    <q-page padding>
+        <div class="q-gutter-sm">
+            <q-btn class="bs-btn dku-text" unelevated>
+                Tooltip Bottom
+                <q-tooltip anchor="bottom middle" self="top middle" class="bs-tooltip">
+                    Chart Name
+                </q-tooltip>
+            </q-btn>
+            <q-btn class="bs-btn dku-text" unelevated>
+                Tooltip Up
+                <q-tooltip anchor="top middle" self="bottom middle" class="bs-tooltip">
+                    Chart Name
+                </q-tooltip>
+            </q-btn>
+            <q-btn class="bs-btn dku-text" unelevated>
+                Tooltip Left
+                <q-tooltip anchor="center left" self="center right" class="bs-tooltip">
+                    Chart Name
+                </q-tooltip>
+            </q-btn>
+            <q-btn class="bs-btn dku-text" unelevated>
+                Tooltip Right
+                <q-tooltip anchor="center right" self="center left" class="bs-tooltip">
+                    Chart Name
+                </q-tooltip>
+            </q-btn>
+
+        </div>
+    </q-page>
+</template>
+<script>
+</script>
