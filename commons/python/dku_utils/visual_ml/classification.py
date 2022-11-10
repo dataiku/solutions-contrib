4b8328d83972f509d164f1ed6084e8582b20694a
Malick KONATE <gkonate.malick@gmail.com>
2022-11-10 02:20:53 +0100

    Update classification.py

--- commons/python/dku_utils/visual_ml/classification.py
+++ commons/python/dku_utils/visual_ml/classification.py
@@ -1,3 +1,6 @@
+from .visual_ml_commons import get_deployed_model_active_version_id
+
+
 def update_ml_task_classification_metric(ml_task_settings, classification_metric):
     """
     Updates the optimization metric set in a ml_task classification model.
@@ -24,3 +27,19 @@ def update_ml_task_classification_metric(ml_task_settings, classification_metric
     ml_task_settings.set_metric(metric=classification_metric)
     ml_task_settings.save()
     pass
+
+
+def get_deployed_model_used_threshold(project, deployed_model_id):
+    """
+    Retrieves the threshold used by the active version of a deployed model .
+    
+    :param project: dataikuapi.dss.project.DSSProject: A handle to interact with a project on the DSS instance.
+    :param: deployed_model_id: str: ID of the deployed model.
+    
+    :returns deployed_model_used_threshold: double: Threshold used by the active version of the deployed model.
+    """
+    deployed_model = project.get_saved_model(deployed_model_id)
+    model_active_version_id = get_deployed_model_active_version_id(project, deployed_model_id)
+    deployed_model_version_details = deployed_model.get_version_details(model_active_version_id)
+    deployed_model_used_threshold = deployed_model_version_details.details["perf"]["usedThreshold"]
+    return deployed_model_used_threshold
