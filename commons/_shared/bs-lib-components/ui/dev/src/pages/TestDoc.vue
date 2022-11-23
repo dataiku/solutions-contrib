aa65aa1efc0aeb6ac0edc6c23e25016fe3652869
anas laaroussi <anaslaaroussi@MBP-de-anas.lan>
2022-11-23 18:09:38 +0100

    documentation code component

---
+++ commons/_shared/bs-lib-components/ui/dev/src/pages/TestDoc.vue
@@ -0,0 +1,12 @@
+<template>
+    <DocExemple file="BsButton/Primary" title="Test"/>
+</template>
+<script>
+    import DocExemple from '../components/DocExemple.vue';
+
+    export default {
+        components: {
+            DocExemple, 
+        }
+    }
+</script>
