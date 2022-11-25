63205d0f1469f5a29edbc27079c7aa64a246de44
anas laaroussi <anaslaaroussi@MBP-de-anas.lan>
2022-11-25 13:14:42 +0100

    routes for layout gallery & empty layout example

--- commons/_shared/bs-lib-components/ui/dev/src/router/routes.js
+++ commons/_shared/bs-lib-components/ui/dev/src/router/routes.js
@@ -1,4 +1,5 @@
 import pages from './pages'
+import layoutGallery from '../assets/layout-gallery'
 
 const docsPages = [
   {
@@ -40,6 +41,16 @@ const routes = [
     component: () => import('layouts/DocLayout.vue'),
     children: docsPages,
   },
+  ...layoutGallery.map(layout => ({
+    path: layout.demoLink,
+    component: () => import(`../gallery/layouts/${layout.path}.vue`),
+    children: [
+      {
+        path: '',
+        component: () => import('../components/page-parts/layout/LayoutGalleryPage.vue')
+      }
+    ]
+  })),
 
   // Always leave this as last one,
   // but you can also remove it
