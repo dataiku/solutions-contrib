09c344f5ed668a9bae19c12cc6ed4b57f6d64a18
anas laaroussi <anaslaaroussi@MBP-de-anas.lan>
2022-11-25 01:43:53 +0100

    bs select multiple doc

--- commons/_shared/bs-lib-components/ui/dev/src/pages/core-components/BsSelectMultiple.vue
+++ commons/_shared/bs-lib-components/ui/dev/src/pages/core-components/BsSelectMultiple.vue
@@ -1,134 +1,30 @@
 <template>
-    <div class="q-pa-md q-gutter-lg" style="max-width: 400px">
-        <p>Default</p>
-        <BsSelect v-model="model" :options="options" outlined clearable multiple use-chips clear-icon="clear">
-            <template v-slot:selected-item="scope">
-                <q-chip
-                    removable
-                    square
-                    @remove="scope.removeAtIndex(scope.index)"
-                    :tabindex="scope.tabindex"
-                    class="q-mr-xs bs-chip__select"
-                    icon-remove="clear"
-                    text-color="white"
-                >
-            
-                    {{ scope.opt }}
-                </q-chip>
-            </template>
-        </BsSelect>
-        <p>With filters</p>
-        <BsSelect 
-            v-model="model" 
-            :options="options" 
-            outlined 
-            clearable 
-            multiple 
-            use-chips 
-            clear-icon="clear" 
-            @filter="filterFn"
-            use-input
-            input-debounce="0"
-            
-        >
-            <template v-slot:selected-item="scope">
-                <q-chip
-                    removable
-                    square
-                    @remove="scope.removeAtIndex(scope.index)"
-                    :tabindex="scope.tabindex"
-                    class="q-mr-xs bs-chip__select"
-                    icon-remove="clear"
-                    text-color="white"
-                >
-            
-                    {{ scope.opt }}
-                </q-chip>
-            </template>
-        </BsSelect>
-        <p>With filters & Hint</p>
-        <BsSelect 
-            v-model="model" 
-            :options="options" 
-            outlined 
-            clearable 
-            multiple 
-            use-chips 
-            clear-icon="clear" 
-            @filter="filterFn"
-            use-input
-            input-debounce="0"
-            hint="Help message if needed"
-        >
-            <template v-slot:selected-item="scope">
-                <q-chip
-                    removable
-                    square
-                    @remove="scope.removeAtIndex(scope.index)"
-                    :tabindex="scope.tabindex"
-                    class="q-mr-xs bs-chip__select"
-                    icon-remove="clear"
-                    text-color="white"
-                >
-            
-                    {{ scope.opt }}
-                </q-chip>
-            </template>
-        </BsSelect>
-        <p>With filters & Hint & Label</p>
-        <BsSelect 
-            v-model="model" 
-            :options="options" 
-            outlined 
-            clearable 
-            multiple 
-            use-chips 
-            clear-icon="clear" 
-            @filter="filterFn"
-            use-input
-            input-debounce="0"
-            hint="Help message if needed"
-            bsLabel="Company"
-        >
-            <template v-slot:selected-item="scope">
-                <q-chip
-                    removable
-                    square
-                    @remove="scope.removeAtIndex(scope.index)"
-                    :tabindex="scope.tabindex"
-                    class="q-mr-xs bs-chip__select"
-                    icon-remove="clear"
-                    text-color="white"
-                >
-            
-                    {{ scope.opt }}
-                </q-chip>
-            </template>
-        </BsSelect>
-    </div>
+    <doc-page-core title="BsSelectMultiple" :description="description" :quasarUrl="quasarURL">
+        <DocExemple file="BsSelectMultiple/Basic" title="Basic"/>
+        <DocExemple file="BsSelectMultiple/Dense" title="Dense"/>
+        <DocExemple file="BsSelectMultiple/Filter" title="With Filter"/>
+        <DocExemple file="BsSelectMultiple/Hint" title="With Hint"/>
+        <DocExemple file="BsSelectMultiple/NoLabel" title="No Label"/>
+    
+    </doc-page-core>
 </template>
 <script>
-import { BsSelect } from 'ui';
-import { ref } from 'vue';
+    import DocExemple from '../../components/DocExemple.vue';
+    import DocPageCore from '../../components/DocPageCore.vue';
 
-const stringOptions = [
-  'Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'
-]
+    export default {
 
-export default {
-    setup() {
-        const options = ref(stringOptions);
-        return {
-            model: ref([]),
-            options,
-            filterFn (val, update, abort) {
-                update(() => {
-                const needle = val.toLowerCase()
-                options.value = stringOptions.filter(v => v.toLowerCase().indexOf(needle) > -1)
-                })
+        components: {
+            DocExemple, 
+            DocPageCore
+        },
+        setup() {
+            const description = "The BsSelect component has two types of selection: single or multiple. This component opens up a menu for the selection list and action. A filter can also be used for longer lists."
+            const quasarURL = "https://quasar.dev/vue-components/select"
+            return {
+                description,
+                quasarURL
             }
-        };
-    },
-    components: { BsSelect }
-}
+        }
+    }
 </script>
