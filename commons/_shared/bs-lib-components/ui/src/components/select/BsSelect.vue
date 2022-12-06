4732367be00ca735b97cf14da6914e9c46a2a011
anas laaroussi <anas.laaroussi@dataiku.com>
2022-12-06 12:10:45 +0100

    add filtering bu default to select

--- commons/_shared/bs-lib-components/ui/src/components/select/BsSelect.vue
+++ commons/_shared/bs-lib-components/ui/src/components/select/BsSelect.vue
@@ -4,16 +4,20 @@
         <QSelect
             ref="bsSelect"
             v-bind="$attrs"
+            :options="bsOptions"
             dropdown-icon="r_expand_more"
             class="bs-select"
             outlined
             dense
+            :use-input="filter"
+            input-debounce="0"
             popup-content-class="bs-select__popup dds-text-400"
             @popup-show="popupShow"
             @popup-hide="popupHide"
             :popup-content-style="popupStyle"
             :label="computedLabel"
             label-color="#CCCCCC"
+            @filter="filterFn"
         >   
             
             <template v-for="(_, slot) in $slots" v-slot:[slot]="scope">
@@ -29,6 +33,9 @@
     data() {
         return {
             width: 0,
+            // TODO : optimize this behaviour of copying options in future releases
+            allOptions: JSON.parse(JSON.stringify(this.$attrs.options)),
+            bsOptions: JSON.parse(JSON.stringify(this.$attrs.options)),
         }
     },
     props: {
@@ -37,6 +44,10 @@
         }, 
         placeHolder: {
             type: String,
+        },
+        filter: {
+            type: Boolean,
+            default: true,
         }
     },
     components: {
@@ -48,6 +59,26 @@
         },
         popupHide() {
             this.width = 0;
+        },
+        filterFn: function(val, update, abort) {
+            update(() => {
+                if (val === '') {
+                    this.bsOptions = this.allOptions;
+                } else {
+                    const needle = val.toLowerCase();
+                    if (this.allOptions.some((element) => {
+                        return typeof element === 'object' 
+                            && !Array.isArray(element)
+                            && element !== null
+                    })) {
+                        // Case where it is an object
+                        this.bsOptions = this.allOptions.filter(v => (v.label || "").toLowerCase().indexOf(needle) > -1);
+                    } else {
+                        // Case primitive types (string)
+                        this.bsOptions = this.allOptions.filter(v => v.toLowerCase().indexOf(needle) > -1);
+                    }    
+                } 
+            });
         }
     },
     computed: {
