963536a4192ff8b699acc23a1b4676ad107939eb
Malick KONATE <gkonate.malick@gmail.com>
2022-12-13 19:44:32 +0100

    Update python_recipe.py

--- commons/python/dku_utils/recipes/python_recipe.py
+++ commons/python/dku_utils/recipes/python_recipe.py
@@ -1,4 +1,6 @@
+import pandas as pd
 from .recipe_commons import get_recipe_settings_and_dictionary
+from ..python_utils.python_scripts import load_python_string_imports_dataframe
 
 
 def set_python_recipe_inputs(project, recipe_name, recipe_inputs):
@@ -40,3 +42,25 @@ def set_python_recipe_outputs(project, recipe_name, recipe_outputs):
     recipe_settings.save()
     print("Python recipe '{}' outputs successfully set!".format(recipe_outputs, recipe_name))
     pass
+
+
+def get_python_recipe_python_dependencies_dataframe(project, recipe_name):
+    """
+    Retrieves a DataFrame containing all python modules dependencies for a project's recipe.
+    :param project: dataikuapi.dss.project.DSSProject: A handle to interact with a project on the DSS instance.
+    :param recipe_name: str: Name of the recipe.
+    :returns: python_dependencies_dataframe: pandas.core.frame.DataFrame: Pandas DataFrame
+        containing information about all the imports done in the scenario python scripts.
+    """
+    print("Retrieving recipe '{}.{}' python dependencies ...".format(project.project_key, recipe_name))
+    PYTHON_DEPENDENCIES_SCHEMA = ["recipe_name", "imported_from",
+                                  "imported", "all_import_information"]
+    recipe_settings, __ = get_recipe_settings_and_dictionary(project, recipe_name, False)
+    recipe_python_script = recipe_settings.data["payload"]
+    python_dependencies_dataframe = load_python_string_imports_dataframe(recipe_python_script)
+    python_dependencies_dataframe["recipe_name"] = recipe_name
+   
+    if len(python_dependencies_dataframe) == 0:
+        python_dependencies_dataframe = pd.DataFrame(columns=PYTHON_DEPENDENCIES_SCHEMA)
+    print("Recipe '{}.{}' python dependencies successfully retrieved!".format(project.project_key, recipe_name))
+    return python_dependencies_dataframe
