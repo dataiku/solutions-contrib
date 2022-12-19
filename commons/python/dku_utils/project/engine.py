26e536ae396dedf4c3c8e65e31f31de53b37f6b5
Malick KONATE <gkonate.malick@gmail.com>
2022-12-19 14:40:38 +0100

    Deletion of this path because the node /flow is equivalent to the node /project
    --> I merged the content of "engine.py" into "flow_commons.py"

--- commons/python/dku_utils/project/engine.py
+++
@@ -1,5 +0,0 @@
-from dataikuapi.dss.project import DSSProject
-
-
-def get_engine_priority(project: DSSProject):
-    return project.get_settings().settings["settings"]["recipeEnginesPreferences"]["enginesPreferenceOrder"]
