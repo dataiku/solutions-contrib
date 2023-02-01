3eaf63fb82067e29595a860281a85015455c0b2d
Lev Kuzan <levko.kuzan@gmail.com>
2023-02-01 19:50:36 +0100

    white spaces

--- commons/_shared/bs-lib-components/ui/dev/src/gallery/layouts/empty-layout.vue
+++ commons/_shared/bs-lib-components/ui/dev/src/gallery/layouts/empty-layout.vue
@@ -1,38 +1,37 @@
 <template>
-    <BsLayoutDefault docTitle="Solution Name" docIcon="/imgs/doc-logo-example.svg">
-      <BsHeader>
-        <div class="dku-medium-title-sb q-pa-md">Header</div>
-      </BsHeader>
-      <BsDrawer>
-        <div class="dku-medium-title-sb q-pa-md">Left Panel</div>
-      </BsDrawer>
-      <BsDocumentation>
-        <p class="dku-small-title">Description 1</p>
-        <p class="dku-text">Use filters to discover your process through the data. 
-                Remove noisy activities through the bottom filters. 
-                Processes displayed in the main screen can be saved as reference and 
-                compared through conformance checks and individual traces.</p>
-        <p class="dku-small-title">Description 2</p>
-        <p class="dku-text">Use filters to discover your process through the data. 
-                Remove noisy activities through the bottom filters. 
-                Processes displayed in the main screen can be saved as reference and 
-                compared through conformance checks and individual traces.</p>
-        <p class="dku-small-title">Description 3</p>
-        <p class="dku-text">Use filters to discover your process through the data. 
-                Remove noisy activities through the bottom filters. 
-                Processes displayed in the main screen can be saved as reference and 
-                compared through conformance checks and individual traces.</p>
-        <p class="dku-small-title">Description 4</p>
-        <p class="dku-text">Use filters to discover your process through the data. 
-                Remove noisy activities through the bottom filters. 
-                Processes displayed in the main screen can be saved as reference and 
-                compared through conformance checks and individual traces.</p>
-        
-      </BsDocumentation>
-      <BsContent>
-        <router-view />
-      </BsContent>
-    </BsLayoutDefault>
+	<BsLayoutDefault docTitle="Solution Name" docIcon="/imgs/doc-logo-example.svg">
+		<BsHeader>
+			<div class="dku-medium-title-sb q-pa-md">Header</div>
+		</BsHeader>
+		<BsDrawer>
+			<div class="dku-medium-title-sb q-pa-md">Left Panel</div>
+		</BsDrawer>
+		<BsDocumentation>
+			<p class="dku-small-title">Description 1</p>
+			<p class="dku-text">Use filters to discover your process through the data.
+				Remove noisy activities through the bottom filters.
+				Processes displayed in the main screen can be saved as reference and
+				compared through conformance checks and individual traces.</p>
+			<p class="dku-small-title">Description 2</p>
+			<p class="dku-text">Use filters to discover your process through the data.
+				Remove noisy activities through the bottom filters.
+				Processes displayed in the main screen can be saved as reference and
+				compared through conformance checks and individual traces.</p>
+			<p class="dku-small-title">Description 3</p>
+			<p class="dku-text">Use filters to discover your process through the data.
+				Remove noisy activities through the bottom filters.
+				Processes displayed in the main screen can be saved as reference and
+				compared through conformance checks and individual traces.</p>
+			<p class="dku-small-title">Description 4</p>
+			<p class="dku-text">Use filters to discover your process through the data.
+				Remove noisy activities through the bottom filters.
+				Processes displayed in the main screen can be saved as reference and
+				compared through conformance checks and individual traces.</p>
+		</BsDocumentation>
+		<BsContent>
+			<router-view />
+		</BsContent>
+	</BsLayoutDefault>
 </template>
 
 <script>
@@ -43,13 +42,13 @@ import BsHeader from "app/../src/components/layout/BsHeader.vue";
 import BsTab from "../../../../src/components/layout/BsTab.vue"
 
 export default {
-  name: "EmptyLayout",
-  components: {
-    BsTab,
-    BsHeader,
-    BsDrawer,
-    BsDocumentation,
-    BsContent
-}
+	name: "EmptyLayout",
+	components: {
+		BsTab,
+		BsHeader,
+		BsDrawer,
+		BsDocumentation,
+		BsContent
+	}
 }
 </script>
