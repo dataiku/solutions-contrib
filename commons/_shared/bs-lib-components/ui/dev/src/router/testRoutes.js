9812c84ff31e8c084884d1bff7ce054f060f8f49
Lev Kuzan <levko.kuzan@gmail.com>
2023-03-16 13:47:10 +0100

    added another test route

--- commons/_shared/bs-lib-components/ui/dev/src/router/testRoutes.js
+++ commons/_shared/bs-lib-components/ui/dev/src/router/testRoutes.js
@@ -1,8 +1,13 @@
-const testPath = "/test_library"
+const testPath = "/test_library";
+const testTableGenericPath = "/test_table_generic";
 
 export default [
     {
         path: testPath,
         component: () => import(`../test/TestView.vue`),
-    }
+    },
+    {
+        path: testTableGenericPath,
+        component: () => import(`../test/BsTableGenericView.vue`),
+    },
 ]
