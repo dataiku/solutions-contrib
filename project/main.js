932a40f9f8e4aa46bc0658aca261a4394296ec1c
Lev Kuzan <levko.kuzan@gmail.com>
2023-05-09 17:28:12 +0200

    add axios and lodash to ext deps

--- project/main.js
+++ project/main.js
@@ -2,6 +2,7 @@ import { Quasar, Notify } from "quasar";
 import "@quasar/extras/material-icons/material-icons.css";
 import "@quasar/extras/material-icons-outlined/material-icons-outlined.css";
 import "@quasar/extras/material-icons-round/material-icons-round.css";
+import "@quasar/extras/mdi-v6/mdi-v6.css";
 import "quasar/src/css/index.sass";
 import "quasar-ui-bs/dist/style.css";
 import "./fonts/fonts.scss";
