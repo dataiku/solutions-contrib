efd7b767c7b7f7c1606e7482acc941dfab3224c5
Lev Kuzan <levko.kuzan@gmail.com>
2023-05-12 14:59:38 +0200

    for loop for clarity

--- commons/python/business_solutions_api/business_solutions_api.py
+++ commons/python/business_solutions_api/business_solutions_api.py
@@ -2,7 +2,8 @@ from flask import Blueprint
 from commons.python.business_solutions_api.dataset_api import dataset_api
 
 def register_child_blueprints(parent: Blueprint, children: list):
-    [parent.register_blueprint(blueprint) for blueprint in children]
+    for blueprint in children:
+        parent.register_blueprint(blueprint=blueprint);
 
 business_solutions_api = Blueprint("business_solutions_api", __name__, url_prefix="/bs_api")
 
