2afad6a71a876b8b06a76a20b08b11c293a69a10
Lev Kuzan <levko.kuzan@gmail.com>
2023-05-12 15:05:29 +0200

    forgotten cache import

--- commons/python/launch_utils.py
+++ commons/python/launch_utils.py
@@ -4,12 +4,12 @@ import os
 sys.path.insert(0,os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 from commons.python.fetch.fetch_project import fetch_route
 from commons.python.business_solutions_api import business_solutions_api
-from commons.python.caching import cache
+# from commons.python.caching import cache
 
 def create_app(app_name: str):
     app = Flask(app_name)
 
-    cache.init_app(app=app)
+    # cache.init_app(app=app)
 
     app.register_blueprint(fetch_route)
     app.register_blueprint(business_solutions_api)
