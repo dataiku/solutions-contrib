47a9ecff2b448b4649986a585635942b968bcd90
Lev Kuzan <levko.kuzan@gmail.com>
2023-05-12 15:00:25 +0200

    removed semicols

--- commons/python/business_solutions_api/business_solutions_api.py
+++ commons/python/business_solutions_api/business_solutions_api.py
@@ -3,7 +3,7 @@ from commons.python.business_solutions_api.dataset_api import dataset_api
 
 def register_child_blueprints(parent: Blueprint, children: list):
     for blueprint in children:
-        parent.register_blueprint(blueprint=blueprint);
+        parent.register_blueprint(blueprint=blueprint)
 
 business_solutions_api = Blueprint("business_solutions_api", __name__, url_prefix="/bs_api")
 
@@ -11,4 +11,4 @@ register_child_blueprints(
     parent=business_solutions_api,
     children=[
     dataset_api
-]);
+])
