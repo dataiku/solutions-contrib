eea4b780096c0bde9a19c5c1c2b9160c193dda6d
Lev Kuzan <levko.kuzan@gmail.com>
2023-05-24 17:06:29 +0200

    added the ability to use filter in the data

--- commons/python/business_solutions_api/dataset_iterator.py
+++ commons/python/business_solutions_api/dataset_iterator.py
@@ -1,13 +1,14 @@
 from typing import Generator, Optional
 from pandas import DataFrame
 from dataikuapi.dss.dataset import DSSDataset
-
+from commons.python.business_solutions_api.improved_dataset import Dataset
 
 class DatasetIterator:
     def __init__(self, dataset: DSSDataset, chunksize=10000, **kwargs) -> None:
         self.chunksize = chunksize
         self._kwargs = kwargs
-        self._dataset = dataset.get_as_core_dataset()
+
+        self._dataset = Dataset("%s.%s" % (dataset.project_key, dataset.dataset_name))
 
     def _create_generator(self) -> Generator[DataFrame, None, None]:
         return self._dataset.iter_dataframes(chunksize=self.chunksize, **self._kwargs);
