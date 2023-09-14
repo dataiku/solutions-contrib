b08338884bc73b4da31ff5980e53ecfffe5a7bcb
anas laaroussi <anas.laaroussi@dataiku.com>
2023-09-14 09:47:45 +0200

    bug fixes

--- bs-infra/webaiku/extension.py
+++ bs-infra/webaiku/extension.py
@@ -13,6 +13,7 @@ logger = logging.getLogger("webaiku")
 
 class WEBAIKU(object):
     BS_API_PREFIX = "/bs_api"
+    API_PREFIX = "/api"
 
     def __init__(self, app: Optional[Flask], relative_path: str):
         self.exec = Execution(relative_path)
@@ -20,13 +21,21 @@ class WEBAIKU(object):
             self.init_flask_app(app)
 
     def init_flask_app(self, app: Flask):
-        serve_blueprint = ServeBlueprint(self.exec)
-        bs_api_blueprints = [DataikuDatasetBlueprint()]
+        ## Only register serve blueprint for DATAIKU_DSS context
+        if self.exec.context == ExecutionContext.DATAIKU_DSS:
+            serve_blueprint = ServeBlueprint(self.exec)
+            app.register_blueprint(serve_blueprint.blueprint)
 
-        app.register_blueprint(serve_blueprint.blueprint)
+        bs_api_blueprints = [DataikuDatasetBlueprint().blueprint]
         self._register_child_bs_blueprints(app, bs_api_blueprints)
 
     def _register_child_bs_blueprints(self, app: Flask, children: List[Blueprint]):
         for blueprint in children:
             blueprint.url_prefix = self.BS_API_PREFIX + blueprint.url_prefix
+            app.register_blueprint(blueprint=blueprint)
+
+    @staticmethod
+    def extend(app: Flask, children: List[Blueprint]):
+        for blueprint in children:
+            blueprint.url_prefix = WEBAIKU.API_PREFIX + blueprint.url_prefix
             app.register_blueprint(blueprint=blueprint)
--- test.py
+++ test.py
@@ -2,5 +2,8 @@ from webaiku.extension import WEBAIKU
 from flask import Flask
 
 
-app = Flask(__name__)
-WEBAIKU(app=app)
+if __name__ == "__main__":
+    app = Flask(__name__)
+    t = WEBAIKU(app=app, relative_path="")
+    print(t.exec.exec_path)
+    app.run(host="localhost", port=5000)
