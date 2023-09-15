d5052cbd4ce970f3d6d0e26b29cfb7f4d1632148
anas laaroussi <anas.laaroussi@dataiku.com>
2023-09-15 08:08:52 +0200

    move components directory

--- commons/python/business_solutions_api/__init__.py
+++
@@ -1,5 +0,0 @@
-from commons.python.business_solutions_api.business_solutions_api import (
-    business_solutions_api,
-    register_child_blueprints,
-)
-from commons.python.business_solutions_api.dataset_api import dataset_api
