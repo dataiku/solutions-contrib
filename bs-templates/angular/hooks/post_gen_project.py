20d64b91429186c564b505d5d15bcd9e10205b23
anas laaroussi <anas.laaroussi@dataiku.com>
2023-09-15 07:48:51 +0200

    finished post generation hook angular

---
+++ bs-templates/angular/hooks/post_gen_project.py
@@ -0,0 +1,18 @@
+import json
+import os
+
+### if code studio template ###
+dss_env_var = "DIP_HOME"
+dss_code_studio_env_var = "DKU_CODE_STUDIO_BROWSER_PATH"
+is_code_studio = dss_env_var in os.environ and dss_code_studio_env_var in os.environ
+
+if is_code_studio:
+    with open("angular.json", "r") as file:
+        angular_json = json.load(file)
+
+    angular_json["projects"]["project"]["architect"]["build"]["options"][
+        "baseHref"
+    ] = os.getenv("{{ cookiecutter.__code_studio_href_env }}")
+
+    with open("angular.json", "w") as file:
+        json.dump(angular_json, file, indent=2)
