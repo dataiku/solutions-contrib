75d8223add93b9f567d7fa50ad59c493516ccf22
anas laaroussi <anas.laaroussi@dataiku.com>
2023-10-12 15:14:55 +0200

    remapping of packages dependencies to new repos

--- bs-templates/angular/hooks/post_gen_project.py
+++ bs-templates/angular/hooks/post_gen_project.py
@@ -1,18 +1,52 @@
 import json
 import os
 
-### if code studio template ###
-dss_env_var = "DIP_HOME"
-dss_code_studio_env_var = "DKU_CODE_STUDIO_BROWSER_PATH"
-is_code_studio = dss_env_var in os.environ and dss_code_studio_env_var in os.environ
+def read_dss_instance_config():
+    config_path = os.path.expanduser("~/.dataiku/config.json")
+    if not os.path.exists(config_path):
+        return
 
-if is_code_studio and os.getenv("{{ cookiecutter.__code_studio_href_env }}"):
-    with open("angular.json", "r") as file:
-        angular_json = json.load(file)
+    try:
+        with open(config_path, "r") as config_file:
+            config: dict = json.load(config_file)
+        dss_instance_name = "{{ cookiecutter.dss_instance }}"
+        dss_instances_creds = config.get("dss_instances", {})
+        if dss_instance_name == "default":
+            dss_instance_name = config.get("default_instance", "")
+        instance_creds = dss_instances_creds.get(dss_instance_name)
+        instance_host = instance_creds.get("url")
 
-    angular_json["projects"]["{{ cookiecutter.__project_slug }}"]["architect"]["build"][
-        "options"
-    ]["baseHref"] = (os.getenv("{{ cookiecutter.__code_studio_href_env }}") + "/")
+        with open("requirements.local.txt", "a") as file:
+            file.write(
+                "\n"
+                + os.path.join(
+                    instance_host, "public/packages/dataiku-internal-client.tar.gz"
+                )
+            )
 
-    with open("angular.json", "w") as file:
-        json.dump(angular_json, file, indent=2)
+    except Exception as err:
+        return
+
+def modify_angular_base_path():
+    ### if code studio template ###
+    dss_env_var = "DIP_HOME"
+    dss_code_studio_env_var = "DKU_CODE_STUDIO_BROWSER_PATH"
+    is_code_studio = dss_env_var in os.environ and dss_code_studio_env_var in os.environ
+
+    if is_code_studio and os.getenv("{{ cookiecutter.__code_studio_href_env }}"):
+        with open("angular.json", "r") as file:
+            angular_json = json.load(file)
+
+        angular_json["projects"]["{{ cookiecutter.__project_slug }}"]["architect"]["build"][
+            "options"
+        ]["baseHref"] = (os.getenv("{{ cookiecutter.__code_studio_href_env }}") + "/")
+
+        with open("angular.json", "w") as file:
+            json.dump(angular_json, file, indent=2)
+
+if __name__ == "__main__":
+    read_dss_instance_config()
+    modify_angular_base_path()
+
+
+
