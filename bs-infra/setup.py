7db799990f0be62bdce4741746579bc727e074a6
Majid Boubrik <abdelmajid.boubrik@dataiku.com>
2024-01-31 15:37:23 +0100

    Fixed python location in non user isolation mode

--- bs-infra/setup.py
+++ bs-infra/setup.py
@@ -9,9 +9,9 @@ with open(join(dirname(__file__), "requirements.txt"), "r") as f:
 
 setuptools.setup(
     name="webaiku",
-    version="0.0.1",
+    version="0.0.2",
     license="Apache 2.0",
-    author="Anas Laaroussi",
+    author="Majid BOUBRIK, Anas Laaroussi",
     description="Web application tools for Dataiku DSS",
     long_description=long_description,
     long_description_content_type="text/markdown",
