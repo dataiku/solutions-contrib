934289ca4fa6a56c7a44d3d379c0d2b019918d55
Guillaume Cassand <guillaume.cassand@dataiku.com>
2024-04-25 15:55:38 +0200

    Remove bs-documentation from repository
    This is an outdated documentation. Better check on dss-doc https://developer.dataiku.com/latest/tutorials/webapps/code-studio/index.html

--- bs-documentation/source/code-studio/deployment/assets/code.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from webaiku.extension import WEBAIKU
-from webapps.{__YOUR_WEBAPPLICATION_FOLDER__}.backend.fetch_api import fetch_api
-
-
-WEBAIKU(app, "webapps/{__YOUR_WEBAPPLICATION_FOLDER__}/dist")
-WEBAIKU.extend(app, [fetch_api])
